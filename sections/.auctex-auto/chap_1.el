0037908cb173122eb0f205525d547590aa42c9f6
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-10-19 13:18:02 +0200

    change langage to italian
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

---
+++ sections/.auctex-auto/chap_1.el
@@ -0,0 +1,8 @@
+(TeX-add-style-hook
+ "chap_1"
+ (lambda ()
+   (LaTeX-add-labels
+    "fig:anatomy1"
+    "fig:anatomy2"))
+ :latex)
+
---
+++ sections/.auctex-auto/chap_2.el
@@ -0,0 +1,7 @@
+(TeX-add-style-hook
+ "chap_2"
+ (lambda ()
+   (LaTeX-add-labels
+    "fig:figure_two"))
+ :latex)
+
