8cf5a11b02caaaaa6f7cbb2a6559f791916d6575
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-10-20 16:39:21 +0200

    fix: cite the article not the image
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

---
+++ sections/.auctex-auto/chap_3.el
@@ -0,0 +1,18 @@
+(TeX-add-style-hook
+ "chap_3"
+ (lambda ()
+   (LaTeX-add-labels
+    "fig:FIGURE_3.1"
+    "fig:FIGURE_3.2"
+    "fig:FIGURE_3.3"
+    "fig:FIGURE_3.4"
+    "fig:FIGURE_3.6"
+    "fig:FIGURE_3.7"
+    "fig:FIGURE_3.8"
+    "fig:FIGURE_3.11"
+    "fig:FIGURE_3.14"
+    "fig:FIGURE_3.15"
+    "fig:FIGURE_3.16"
+    "fig:FIGURE_3.17"))
+ :latex)
+
