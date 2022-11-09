fac8c021b41a1d9f6e21d0339d76655a3dcd6575
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-11-09 23:39:34 +0100

    Revert "fix subfigure"
    This reverts commit 15b202ec5873edeae61fc5240f039b7657701e57.

--- .auctex-auto/thesis.el
+++ .auctex-auto/thesis.el
@@ -4,30 +4,29 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("report" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("geometry" "top=1in" "bottom=1in" "left=1.4in" "right=1in") ("inputenc" "utf8") ("changepage" "strict") ("fontenc" "T1") ("caption" "font=small" "skip=0pt") ("dirtytalk" "left=\\flqq{}" "right=\\frqq{}" "leftsub=\\flq{}" "rightsub=\\frq{}")))
+                     '(("babel" "italian") ("geometry" "top=1in" "bottom=1in" "left=1.4in" "right=1in") ("inputenc" "utf8") ("changepage" "strict") ("fontenc" "T1") ("caption" "font=small" "skip=0pt") ("dirtytalk" "left=\\flqq{}" "right=\\frqq{}" "leftsub=\\flq{}" "rightsub=\\frq{}")))
    (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "format/title_page"
-    "sections/blankpage"
     "sections/abstract"
+    "sections/Conclusioni"
+    "sections/Ringraziamenti"
     "sections/chap_1"
     "sections/chap_2"
     "sections/chap_3"
     "sections/chap_4"
     "sections/chap_5"
-    "sections/Conclusioni"
-    "sections/Ringraziamenti"
     "report"
     "rep11"
     "babel"
@@ -43,28 +42,24 @@
     "blindtext"
     "enumitem"
     "amsthm"
-    "tikz"
-    "graphics"
-    "color"
-    "fullpage"
-    "float"
-    "epsf"
-    "caption"
-    "subcaption"
+    "subfig"
     "listings"
     "listingsutf8"
     "amsmath"
     "framed"
     "minibox"
+    "float"
     "wrapfig"
     "longtable"
     "changepage"
     "pgfplots"
+    "tikz"
     "hyperref"
     "fontenc"
     "lipsum"
     "tabularx"
     "ltablex"
+    "caption"
     "dirtytalk")
    (TeX-add-symbols
     "forceindent"
--- sections/.auctex-auto/chap_2.el
+++ sections/.auctex-auto/chap_2.el
@@ -2,13 +2,6 @@
  "chap_2"
  (lambda ()
    (LaTeX-add-labels
-    "fig:FIGURE_2.2"
-    "fig:FIGURE_2.3"
-    "fig:FIGURE_2.13"
-    "fig:FIGURE_2.14"
-    "fig:FIGURE_2.16"
-    "fig:FIGURE_2.17"
-    "fig:FIGURE_2.18"
-    "fig:FIGURE_2.19"))
+    "fig:figure_two"))
  :latex)
 
